cf262bdad6e71abbc621bb45ddb4d92e3c603d20
zbennett10 <zbennett10@Hotmail.com>
2016-11-07 20:00:28 -0500

    Add project files.

---
+++ WeatherAppComplete/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WeatherAppComplete
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private async void Search_Weather(object sender, RoutedEventArgs e)
+        {
+            string cityName = searchBox.Text;
+            textBlockCity.Text = cityName;
+            RootObject currentWeather = await Proxy.GetWeather(cityName);
+            FiveDayForecast Forecast = new FiveDayForecast(currentWeather);
+
+
+            ////Day0
+            icon0.Source = Forecast.fiveDayForecastArr[0].IconSource;
+            date0.Text = Forecast.fiveDayForecastArr[0].Date;
+            maxTemp0.Text = Forecast.fiveDayForecastArr[0].Temp;
+
+            ////Day1
+            icon1.Source = Forecast.fiveDayForecastArr[1].IconSource;
+            date1.Text = Forecast.fiveDayForecastArr[1].Date;
+            maxTemp1.Text = Forecast.fiveDayForecastArr[1].Temp;
+
+
+            ////Day2
+            icon2.Source = Forecast.fiveDayForecastArr[2].IconSource;
+            date2.Text = Forecast.fiveDayForecastArr[2].Date;
+            maxTemp2.Text = Forecast.fiveDayForecastArr[2].Temp;
+
+
+            ////Day3
+            icon3.Source = Forecast.fiveDayForecastArr[3].IconSource;
+            date3.Text = Forecast.fiveDayForecastArr[3].Date;
+            maxTemp3.Text = Forecast.fiveDayForecastArr[3].Temp;
+
+            ////Day4
+            icon4.Source = Forecast.fiveDayForecastArr[4].IconSource;
+            date4.Text = Forecast.fiveDayForecastArr[4].Date;
+            maxTemp4.Text = Forecast.fiveDayForecastArr[4].Temp;  
+        }
+    }
+}
---
+++ WeatherAppComplete/Proxy.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.IO;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace WeatherAppComplete
+{
+    class Proxy
+    {
+        public async static Task<RootObject> GetWeather(string cityName)
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync(String.Format("http://api.openweathermap.org/data/2.5/forecast?q={0}&units=imperial&appID=156ce1f2ff11af0169f79b186098b7a6", cityName));
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(memoryStream);
+
+            return data;
+
+        }
+    }
+
+
+
+    public class FiveDayForecast 
+    {
+
+        public RootObject currentWeather { get; set; }
+        public DailyForecast[] fiveDayForecastArr { get; set; }
+  
+        public FiveDayForecast(RootObject weather)
+        {
+            this.currentWeather = weather;
+            this.fiveDayForecastArr = FiveDayForecaster();
+        }
+
+        public DailyForecast[] FiveDayForecaster()
+        {
+            
+            DailyForecast dayOne = new DailyForecast(this.currentWeather.list[0].weather[0].icon, this.currentWeather.list[0].dt_txt, this.currentWeather.list[0].main.temp);
+            DailyForecast dayTwo = new DailyForecast(this.currentWeather.list[8].weather[0].icon, this.currentWeather.list[8].dt_txt, this.currentWeather.list[8].main.temp);
+            DailyForecast dayThree = new DailyForecast(this.currentWeather.list[16].weather[0].icon, this.currentWeather.list[16].dt_txt, this.currentWeather.list[16].main.temp);
+            DailyForecast dayFour = new DailyForecast(this.currentWeather.list[24].weather[0].icon, this.currentWeather.list[24].dt_txt, this.currentWeather.list[24].main.temp);
+            DailyForecast dayFive = new DailyForecast(this.currentWeather.list[32].weather[0].icon, this.currentWeather.list[32].dt_txt, this.currentWeather.list[32].main.temp);
+            DailyForecast[] fiveDayForecast = { dayOne, dayTwo, dayThree, dayFour, dayFive };
+
+            return fiveDayForecast;
+        }
+
+    }
+
+
+    public class DailyForecast
+    {
+        public BitmapImage IconSource { get; set; }
+        public string Date { get; set; }
+        public string Temp { get; set; }    
+
+        public DailyForecast(string icon, string date, double temp)
+        {
+            this.IconSource = new BitmapImage(new Uri(String.Format("http://openweathermap.org/img/w/{0}.png", icon)));
+            this.Date = DateTime.Parse(date).ToString("ddd");
+            this.Temp = String.Format("{0}°F ", Math.Round(temp).ToString());    
+        }
+
+    }
+
+
+
+
+
+
+    /*[DataContract]
+    public class Coord
+    {
+        [DataMember]
+        public double lon { get; set; }
+        [DataMember]
+        public double lat { get; set; }
+    }
+
+    [DataContract]
+    public class Sys
+    {
+        [DataMember]
+        public int population { get; set; }
+    }*/
+
+    [DataContract]
+    public class City
+    {
+        [DataMember]
+        public int id { get; set; }
+
+        [DataMember]
+        public string name { get; set; }
+
+        /*[DataMember]
+        public Coord coord { get; set; }*/
+
+        [DataMember]
+        public string country { get; set; }
+
+        [DataMember]
+        public int population { get; set; }
+
+        /*[DataMember]
+        public Sys sys { get; set; }*/
+    }
+
+    [DataContract]
+    public class Main
+    {
+        [DataMember]
+        public double temp { get; set; }
+
+        [DataMember]
+        public double temp_min { get; set; }
+
+        [DataMember]
+        public double temp_max { get; set; }
+
+        [DataMember]
+        public double pressure { get; set; }
+
+        [DataMember]
+        public double sea_level { get; set; }
+
+        [DataMember]
+        public double grnd_level { get; set; }
+
+        [DataMember]
+        public int humidity { get; set; }
+
+        [DataMember]
+        public double temp_kf { get; set; }
+    }
+
+    [DataContract]
+    public class Weather
+    {
+        [DataMember]
+        public int id { get; set; }
+
+        [DataMember]
+        public string main { get; set; }
+
+        [DataMember]
+        public string description { get; set; }
+
+        [DataMember]
+        public string icon { get; set; }
+    }
+
+    [DataContract]
+    public class Clouds
+    {
+        [DataMember]
+        public int all { get; set; }
+    }
+
+    [DataContract]
+    public class Wind
+    {
+        [DataMember]
+        public double speed { get; set; }
+
+        [DataMember]
+        public double deg { get; set; }
+    }
+
+    [DataContract]
+    public class Rain
+    {
+
+        [DataMember]
+        public double __invalid_name__3h { get; set; }
+    }
+
+    /*[DataContract]
+    public class Sys2
+    {
+        [DataMember]
+        public string pod { get; set; }
+    }*/
+
+    [DataContract]
+    public class List
+    {
+        [DataMember]
+        public int dt { get; set; }
+
+        [DataMember]
+        public Main main { get; set; }
+
+        [DataMember]
+        public List<Weather> weather { get; set; }
+
+        [DataMember]
+        public Clouds clouds { get; set; }
+
+        [DataMember]
+        public Wind wind { get; set; }
+
+        [DataMember]
+        public Rain rain { get; set; }
+
+        /*[DataMember]
+        public Sys2 sys { get; set; }*/
+
+        [DataMember]
+        public string dt_txt { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public City city { get; set; }
+
+        [DataMember]
+        public string cod { get; set; }
+
+        [DataMember]
+        public double message { get; set; }
+
+        [DataMember]
+        public int cnt { get; set; }
+
+        [DataMember]
+        public List<List> list { get; set; }
+    }
+
+
+
+}
